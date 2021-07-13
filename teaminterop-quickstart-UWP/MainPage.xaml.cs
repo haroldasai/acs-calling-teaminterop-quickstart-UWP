1909413f8ce6052b7ad21fe2a58b0ad748a59726
Harold Asai <koichiroasai@gmail.com>
2021-07-13 10:58:22 -0700

    Add project files.

---
+++ teaminterop-quickstart-UWP/MainPage.xaml.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Azure.Communication;
+using Azure.Communication.Calling;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace teaminterop_quickstart_UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        string user_token_ = "REPLACE_WITH_YOUR_ACS_USER_ACCESS_TOKEN";
+
+        Call call_;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void JoinButton_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            if (!await ValidateInput())
+            {
+                return;
+            }
+
+            //
+            //  Create CallClient
+            //
+            CallClient call_client = new CallClient();
+
+            //
+            //  Create CallAgent
+            //
+            CommunicationTokenCredential token_credential;
+            CallAgent call_agent;
+
+            try
+            {
+                token_credential = new CommunicationTokenCredential(user_token_);
+
+                CallAgentOptions call_agent_options = new CallAgentOptions();
+                call_agent = await call_client.CreateCallAgent(token_credential, call_agent_options);
+            }
+            catch
+            {
+                await new MessageDialog("It was not possible to create call agent. Please check if token is valid.").ShowAsync();
+                return;
+            }
+
+            //
+            //  Join a Teams meeting
+            //
+            try
+            {
+                JoinCallOptions joinCallOptions = new JoinCallOptions();
+                TeamsMeetingLinkLocator teamsMeetingLinkLocator = new TeamsMeetingLinkLocator(TeamsLinkTextBox.Text);
+                call_ = await call_agent.JoinAsync(teamsMeetingLinkLocator, joinCallOptions);
+            }
+            catch
+            {
+                await new MessageDialog("It was not possible to join the Teams meeting. Please check if Teams Link is valid.").ShowAsync();
+                return;
+            }
+
+            //
+            //  Set up call callbacks
+            //
+            call_.OnStateChanged += Call_OnStateChangedAsync;
+            call_.OnIsRecordingActiveChanged += Call_OnIsRecordingActiveChangedAsync;
+            call_.OnRemoteParticipantsUpdated += Call__OnRemoteParticipantsUpdated;
+        }
+
+        private async void Call__OnRemoteParticipantsUpdated(object sender, ParticipantsUpdatedEventArgs args)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                foreach (var remoteParticipant in args.AddedParticipants)
+                {
+                    ParticipantsTextBlock.Text += remoteParticipant.DisplayName.ToString() + "\n";
+                }
+            });
+        }
+
+        private async void Call_OnStateChangedAsync(object sender, PropertyChangedEventArgs args)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                CallStatusTextBlock.Text = call_.State.ToString();
+            });
+        }
+
+        private async void Call_OnIsRecordingActiveChangedAsync(object sender, PropertyChangedEventArgs args)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                RecordingStatusTextBlock.Text = call_.IsRecordingActive ? "Recording is active." : "Recording is inactive.";
+            });
+        }
+
+
+        private async void LeaveButton_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await call_.HangUpAsync(new HangUpOptions());
+            }
+            catch
+            {
+                await new MessageDialog("It was not possible to leave the Teams meeting.").ShowAsync();
+            }
+        }
+
+        private async Task<bool> ValidateInput()
+        {
+            if (user_token_.StartsWith("<"))
+            {
+                await new MessageDialog("Please enter token in source code.").ShowAsync();
+                return false;
+            }
+
+            if (TeamsLinkTextBox.Text.Trim().Length == 0 || !TeamsLinkTextBox.Text.StartsWith("http"))
+            {
+                await new MessageDialog("Please enter Teams meeting link.").ShowAsync();
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+}
